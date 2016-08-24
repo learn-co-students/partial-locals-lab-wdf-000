4ea3578ca983d383011454d696b97e2ed3bd14f9
Derick Quartey <niikwartey1@gmail.com>
2016-08-24 13:39:29 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
       render 'new'
     end
   end
-
+  
   def edit
     @student = Student.find(params[:id])
   end
--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,9 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    name_regex = name.strip.empty? ? /./ : /#{name.downcase}/
+    self.all.select {|student| student.name.downcase.match(name_regex) }
+  end
 end
