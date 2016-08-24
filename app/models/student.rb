a9a70ea8e374d0d39a517cd2341672a080996b28
Margaret <eutopia@gmail.com>
2016-08-24 15:30:09 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,8 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(keyword)
+  	Student.all.select {|student| student.name.downcase.match(/#{keyword}/)}
+  end
 end
