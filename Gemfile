18f18fa037234d55591fe6e9b5af132cb30cf14f
aehmt <calisahmet@gmail.com>
2016-08-28 22:38:01 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'rspec-rails',      ">= 2.0.0.beta"
   gem 'annotate'
+  gem 'pry'
   gem 'pry-byebug'
   gem 'faker'
 end
--- app/models/classroom_student.rb
+++ app/models/classroom_student.rb
@@ -1,13 +1,12 @@
+# Table name: classroom_students
 # == Schema Information
 #
-# Table name: classroom_students
 #
 #  id           :integer          not null, primary key
 #  classroom_id :integer
 #  student_id   :integer
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
-#
 
 class ClassroomStudent < ActiveRecord::Base
   belongs_to :classroom
