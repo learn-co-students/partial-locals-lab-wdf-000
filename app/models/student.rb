78d3cb3a5905ab1de732ed97b2625392be985261
Flatiron School <ellis.paige.v@gmail.com>
2016-09-22 20:25:24 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query.empty?
+      all
+    else
+      all.select {|student| student.name.downcase.include?(query.downcase)}
+    end
+  end
 end
