3abcfd9be9b7c841cc14b96247c01f6c533e3cc1
Wendy Xu <guangw.xu@gmail.com>
2016-09-30 15:46:51 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,8 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.serach(search_string)
+    render 'index'
   end
 
   def student_params
