d801c8d9b125a3f80699c23c72c114228fea393e
Isuru <github email address>
2016-10-11 15:19:40 -0400

    Done.

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,10 +1,13 @@
 class ClassroomsController < ApplicationController
   def show
-    @classroom = Classroom.find(params[:id])
-    @student = @classroom.oldest_student
+    # @classroom = Classroom.find(params[:id])
+    # @student = @classroom.oldest_student
+    # @students = @classroom.students
+    # render :show
   end
 
   def index
     @classrooms = Classroom.all
   end
+
 end
