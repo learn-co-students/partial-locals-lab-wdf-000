c85d31c8ae82a975a0de1cdffc9d6a5028d846a6
parm530 <parmsahadeo530@gmail.com>
2016-10-12 16:22:48 -0400

    Done

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,16 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    if name.empty?
+      self.all
+    else
+      names = self.all.map{|student| student.name.downcase}.grep(/#{name}/)
+      names.collect do |name|
+        self.find_by(name: name.capitalize)
+      end
+    end
+  end
+
 end
